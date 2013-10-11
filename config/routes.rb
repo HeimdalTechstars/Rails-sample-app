21144210235f4f9628c1e27c58de4acfcfcf9651
Heimdal Techstars <heimdaltechstars@Heimdals-Mac-mini.local>
2013-10-11 14:15:10 +0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 SampleApp::Application.routes.draw do
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   get "users/new"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   resources :microposts, only: [:create, :destroy]
   root 'static_pages#home'
   match '/signup', to: 'users#new', via: 'get'
